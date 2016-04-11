e04c5e69c73a4106fa7dfe0db45cbaa7a03414af
Penina Wanjiru <penina.wanjiru@andela.com>
2016-04-12 00:28:59 +0300

    added macros

--- app.py
+++ app.py
@@ -4,20 +4,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def template_test():
-    return render_template('macro.py', my_string="This is awesome", my_list=["mangos","pineapples","strawberries","pears","oranges"])
+    return render_template('macro.html', my_string="Best fruits", my_list=["mangos","pineapples","strawberries","pears","oranges"])
+
 
 @app.route("/home")
 def home():
-    return render_template('macro.py', my_string="Wheeeee!", my_list=[0,1,2,3,4,5], title="home")
+    return render_template('macro.html', my_string="Amazing games!", my_list=["skydiving","swimming","hockey","cycling","running"], title="home")
+
 
 @app.route("/about")
 def about():
-    return render_template('macro.py', my_string="Wheeeee!", my_list=[0,1,2,3,4,5], title="About")
+    return render_template('macro.html', my_string="Everything cool!", my_list=[0,1,2,3,4,5], title="About")
 
 
 @app.route("/contact")
 def contact():
-    return render_template('macro.py', my_string="Wheeeee!", my_list=[0,1,2,3,4,5], title="Contact Us")
+    return render_template('macro.html', my_string="TIA!", my_list=[0,1,2,3,4,5], title="Contact Us")
 
 if __name__ == '__main__':
     app.run(debug=True)
